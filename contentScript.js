48acd4f0548d2c720ef147cb66594f225a470f95
Nikita Suyazov <jumper211212@gmail.com>
2023-03-19 18:57:57 +0200

    add animations

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,18 @@
+const validUrlChunks = ['/c/', '/channel/', '/user/', '/@', '/watch?v='];
+const isValidUrl = (url) => validUrlChunks.some((chunk) => url.includes(chunk));
+
 const replaceProtocol = (url) => url.replace('http:', 'https:');
 
 const extractString = ({ bodyText, startString, endString }) => (
   bodyText.match(`(?<=${startString}).*?(?=${endString})`)[0] || null
 );
+
+let fetchController = new AbortController();
+
+const abortPreviousFetch = () => {
+  fetchController.abort();
+  fetchController = new AbortController();
+};
 
 const getChannelUrl = (videoUrl) => {
   const { signal } = fetchController;
@@ -57,31 +67,52 @@ const getSubscriberCountElement = (isVideoPageUrl) => new Promise((resolve) => {
   observer.observe(document.body, { childList: true, subtree: true });
 });
 
-const createMonetizationStatusElement = (isMonetized, isVideoPageUrl) => {
-  const textColor = isMonetized ? '#4CBB17' : '#D22B2B';
+const createMonetizationStatusLoadingElement = (isVideoPageUrl) => {
   const fontSize = isVideoPageUrl ? '1.2rem' : '1.4rem';
-  const style = `'font-size: ${fontSize}; font-weight: 400; color:${textColor}'`;
-  const monetizationStatusText = isMonetized ? 'Channel is monetized' : 'Channel is not monetized';
+  const element = document.createElement('div');
+  element.id = 'monetization-status';
+  element.className = 'monetization-status-loading';
+  element.style.cssText += `font-size: ${fontSize}; font-weight: 400; color: #ffff00`;
+  element.textContent = 'Monetization status is loading';
 
-  return `<div id='monetization-status' style=${style}>${monetizationStatusText}</div>`;
+  return element;
 };
 
-let fetchController = new AbortController();
-
-chrome.runtime.onMessage.addListener(async ({ url }) => {
-  fetchController.abort();
-  fetchController = new AbortController();
+const insertMonetizationStatusLoadingElement = async (isVideoPageUrl) => {
+  const subscriberCountElement = await getSubscriberCountElement(isVideoPageUrl);
+  const monetizationStatusLoadingElement = createMonetizationStatusLoadingElement(isVideoPageUrl);
 
   document.getElementById('monetization-status')?.remove();
+  subscriberCountElement.after(monetizationStatusLoadingElement);
 
-  const isVideoPageUrl = url.includes('watch?v=');
-  const [isMonetized, subscriberCountElement] = await Promise.all([
-    checkIsMonetized(url, isVideoPageUrl),
-    getSubscriberCountElement(isVideoPageUrl),
+  return monetizationStatusLoadingElement;
+};
+
+const updateMonetizationStatusElement = (monetizationStatusElement, isMonetized) => {
+  const textColor = isMonetized ? '#4CBB17' : '#D22B2B';
+  const monetizationStatusTextMap = new Map([
+    [true, 'Channel is monetized'],
+    [false, 'Channel is not monetized'],
+    [null, 'Monetization status unknown'],
   ]);
 
-  if (isMonetized === null) return;
+  monetizationStatusElement.className = 'monetization-status-loaded';
 
-  const monetizationStatusElement = createMonetizationStatusElement(isMonetized, isVideoPageUrl);
-  subscriberCountElement.insertAdjacentHTML('afterEnd', monetizationStatusElement);
+  setTimeout(() => {
+    monetizationStatusElement.style.cssText += `color: ${textColor}`;
+    monetizationStatusElement.textContent = monetizationStatusTextMap.get(isMonetized);
+  }, 500);
+};
+
+chrome.runtime.onMessage.addListener(async ({ url }) => {
+  if (!isValidUrl(url)) return;
+  abortPreviousFetch();
+
+  const isVideoPageUrl = url.includes('watch?v=');
+  const [isMonetized, monetizationStatusLoadingElement] = await Promise.all([
+    checkIsMonetized(url, isVideoPageUrl),
+    insertMonetizationStatusLoadingElement(isVideoPageUrl),
+  ]);
+
+  updateMonetizationStatusElement(monetizationStatusLoadingElement, isMonetized);
 });
